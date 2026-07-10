ed517788a08b483619babb45b715556c14eb1742
Vanshika Babral <vanshikababral@gmail.com>
2026-07-10 12:09:46 +0530

    Added WebpageLoader

--- directory_loader.py
+++ directory_loader.py
@@ -2,9 +2,9 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import PyPDFLoader
 
 loader = DirectoryLoader("Sample_doc/", glob="**/*.pdf", loader_cls=PyPDFLoader, show_progress=True)
-documents = loader.load()
+documents = loader.lazy_load()
 
-print(f"Number of Documents: {len(documents)}")
+# print(f"Number of Documents: {len(documents)}")
 
 for idx, document in enumerate(documents, start=1):
     print(f"\nDocument {idx}")
