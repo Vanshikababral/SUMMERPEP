48b653363014ca2e24d48a8fd87360811bde6878
Vanshika Babral <vanshikababral@gmail.com>
2026-07-10 11:38:11 +0530

    Added DirectoryLoader

---
+++ directory_loader.py
@@ -0,0 +1,13 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = DirectoryLoader("Sample_doc/", glob="**/*.pdf", loader_cls=PyPDFLoader, show_progress=True)
+documents = loader.load()
+
+print(f"Number of Documents: {len(documents)}")
+
+for idx, document in enumerate(documents, start=1):
+    print(f"\nDocument {idx}")
+    print("Content:\n", document.page_content)
+    print("Metadata:\n", document.metadata)
+
