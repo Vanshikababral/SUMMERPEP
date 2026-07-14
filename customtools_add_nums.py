d4ca518dfb42544eddb362ae128dc4da241ba68e
Vanshika Babral <vanshikababral@gmail.com>
2026-07-14 12:56:20 +0530

    Added weather_of_City in Custom Tools

--- customtools_add_nums.py
+++ customtools_add_nums.py
@@ -1,10 +1,16 @@
 from langchain_core.tools import tool
 
 @tool 
-def add(num1: int, num2:int):
+def add(num1: int, num2:int)->int:
+    """Adds two integers and returns the sum."""
     return num1 + num2
 
+@tool
+def weather_of_Jalandhar(text:str)->int:
+    """This return the current temperatre of City"""
+    return 36
 
-result = add.invoke({"num1" : 12, "num2" : 23 })
+
+result = weather_of_Jalandhar.invoke({"text": "Jalandhar"})
 print(result)
 
